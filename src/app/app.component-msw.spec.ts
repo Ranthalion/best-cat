66509810c7e381a6f77dab02c46840934483763a
ranthalion <ranthalion@hotmail.com>
2023-05-14 17:20:46 -0400

    don't bother with unit tests

--- src/app/app.component-msw.spec.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { mswServer } from 'src/mocks/server';
-
-beforeEach(() => mswServer.listen());
-afterEach(() => mswServer.resetHandlers());
-afterAll(() => mswServer.close());
-
-//TODO: [ML] Define some sample tests
